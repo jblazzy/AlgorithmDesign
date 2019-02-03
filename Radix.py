ad06b650b280400c8894b36b8862a56fce55a679
jblazzy <blaz_j1@denison.edu>
2019-02-03 15:09:52 -0500

    working on Radix

--- Radix.py
+++ Radix.py
@@ -8,8 +8,14 @@ L - unsorted list of numbers
 M - upper bound of the range of number in the list (0...M)
 Returns sorted list s.
 '''
-def Bucket(L, M):
-
+def Bucket(L):
+    # Should we find the max first and set that equal to M?
+    maxx = 0
+    for item in L:
+        if item > maxx:
+            maxx = item
+    
+    M = maxx + 1
     # Initialize bucket list and sorted list to all 0's
     s = []
     bucket = []
@@ -20,7 +26,6 @@ def Bucket(L, M):
 
     for i in L:
         bucket[i] = bucket[i] + 1
-    print("bucket:", bucket) # seems to be working
 
     i = 0
     for j in range(0, M):
@@ -29,29 +34,49 @@ def Bucket(L, M):
             i = i + 1
 
     return s
-
+'''
+Radix Sort Algorith.
+L - Unsorted list of strings.
+k - Upper bound of the length of each string in the list.
+Returns a sorted list of strings.
 '''
 def Radix(L, k):
-    # Strings must be exactly length k.
+    # Strings must be exactly length k. (k digits)
+    ret = [] # list to be returned
+    ret_ascii = []
+    combined = []
 
-    for word in list:
-        if len(word) > k:
-            remove
+    for word in L:
         if len(word) < k:
-            # add 0's to make length of words shorter than k equal to length k
-            for i in range(0,(k-len(word))):
-                word = word + '0'
+            # add 0's to make word length equal to k
+            word = word + ('0' * (k-len(word)))
+            ret.append(word)
+        elif len(word) == k:
+            ret.append(word)
 
-    for i in range(k,1):
-        Bucket(L,6)
+    for i in range(k-1,-1,-1):
+        temp = []
+        for word in ret_ascii:
+            temp.append(word[i])
 
-'''
+        temp = Bucket(temp)
+
+
+
+    #print(combined)
+    #print(temp)
+    return(ret)
 
 def main():
-    unsorted = [3,1,4,1,1,5]
+    '''
+    unsorted = [3,1,4,1,1,5, 9, 10, 8, 9, 4]
     print("input:", unsorted)
-    print("output:", Bucket(unsorted, 6))
+    print("output:", Bucket(unsorted))
     print("should be: [1,1,1,3,4,5]")
+    '''
+    
+    namelist = ["robb", "jon","rickon", "bran"]
+    Radix(namelist, 6)
 
 if __name__ == "__main__":
     main()
