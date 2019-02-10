a3cf37b8c648f1a556b7d5c3b565b8e20d46137f
jblazzy <blaz_j1@denison.edu>
2019-02-10 13:39:43 -0500

    Finished input, working on alg.

--- HW2/GaleShapley.py
+++ HW2/GaleShapley.py
@@ -2,22 +2,36 @@
 ## 2/6/19
 ## Gale Shapley Algorithm
 
-def GS():
+'''
+Function that implements the Gale-Shapley Algorithm.
+men - men's preference dictionary taken from input.
+women - women's preference dictionary taken from input.
+Returns a stable matching.
+'''
+def GS(men, women):
     #Create inverted list for each woman
     ranks = {}  
 
 def In():
     n = int(input()) ## Number of iterations given by input (the following n inputs are to be sorted)
-                     ## Convert user str input to int
 
+    men = {}
+    women = {}
+
+    print("Men preference array:")
     for i in range(0, n):
-        men = {}
-        women = {}
+        split = input().split(" ")
+        men[split[0]] = split[1:]
+    
+    print("Women preference array:")
+    for i in range(0, n):
+        split = input().split(" ")
+        women[split[0]] = split[1:]
 
-        
-        n = int(input()) ## Take next n for next set of name inputs
-        if n == 0:       ## End input once a '0' is received     
-            break
+    print("men: ",men)
+    print("women: ",women)
+    
+    return men, women
 
 def main():    
     '''
@@ -26,12 +40,12 @@ def main():
     ## Women's preferences
     women = [['A','B','C'],['Y','Z','Z'],['X','Y','X'],['Z','X','Y']]
     '''
-    In()
-    #GS()
+    men, women = In()
+
+    GS(men,women)
 
 
     
 if __name__ == "__main__":
     main()
-
 
