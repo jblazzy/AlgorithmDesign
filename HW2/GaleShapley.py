0af3897b7852c06276828199cc8b482e57d0f7eb
jblazzy <blaz_j1@denison.edu>
2019-02-10 13:26:20 -0500

    Working on GS input

---
+++ HW2/GaleShapley.py
@@ -0,0 +1,37 @@
+## Josh Blaz
+## 2/6/19
+## Gale Shapley Algorithm
+
+def GS():
+    #Create inverted list for each woman
+    ranks = {}  
+
+def In():
+    n = int(input()) ## Number of iterations given by input (the following n inputs are to be sorted)
+                     ## Convert user str input to int
+
+    for i in range(0, n):
+        men = {}
+        women = {}
+
+        
+        n = int(input()) ## Take next n for next set of name inputs
+        if n == 0:       ## End input once a '0' is received     
+            break
+
+def main():    
+    '''
+    ## Men's preferences
+    men = [['X','Y','Z'],['A','A','A'],['B','C','B'],['C','B','C']]
+    ## Women's preferences
+    women = [['A','B','C'],['Y','Z','Z'],['X','Y','X'],['Z','X','Y']]
+    '''
+    In()
+    #GS()
+
+
+    
+if __name__ == "__main__":
+    main()
+
+
