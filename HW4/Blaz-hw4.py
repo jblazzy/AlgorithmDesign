844e891ea0266a0fa48819396f94059fd0e8ec92
jblazzy <blaz_j1@denison.edu>
2019-03-07 11:17:41 -0500

    merge function

--- HW4/Blaz-hw4.py
+++ HW4/Blaz-hw4.py
@@ -2,3 +2,67 @@
 ## 3/7/19
 ## Sort and Count Algorithm
 
+
+'''
+Function that implements the Sort and Count Algorithm.
+L - List of integers.
+Returns a sorted list and the number of inversions in the list.
+'''
+def SC(L):
+    iL = 0 # Left inversions
+    iR = 0 # Right inversions
+    iB = 0 # Between (combined) inversions
+
+    if len(L)==1:
+        return(0,L)
+
+    LL,LR = split(L) # Split evenly into two lists (Left and Right)
+
+
+'''
+Function that merges and counts inversions.
+Returns the list and the inversions between.
+'''
+def Merge(LL,LR):
+    L = [] # Solution
+    i = 0
+    j = 0
+    iB = 0 
+
+    while(i < len(LL) and j < len(LR)):
+        L.append(min(LL[i],LR[i])) # Append the smaller of LL[i] and LR[i] to L
+        
+        if(LL[i] > LR[j]):
+            iB += len(LL) - i
+            i += 1
+        else:
+            iB += len(LR) - j
+            j += 1
+
+    return(iB, L)
+
+
+'''
+Function that splits a list in half.
+L - List of integers.
+Returns two halves of a list.
+'''
+def split(L):
+    temp1 = [] # First half
+    temp2 = [] # First half
+
+    temp1 = L[:len(L)//2]
+    temp2 = L[len(L)//2:]
+    return temp1,temp2
+
+
+def main():
+    L = [1,2,3,4,5,6,7,8]
+
+    print(L)
+
+    print(split(L))
+
+if __name__ == "__main__":
+    main()
+
